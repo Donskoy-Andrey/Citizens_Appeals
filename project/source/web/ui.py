194cd03b1aa65801e3995e7147cf4c2b47cc038f
Artem Medvedev <artiebears@mail.ru>
2023-11-23 20:40:00 +0300

    init

--- project/source/web/ui.py
+++ project/source/web/ui.py
@@ -12,42 +12,27 @@ class Gui:
         Initialize the class Gui
         """
         st.set_page_config(layout="wide")
-
-        self.uploaded_file = None
-        self.button_disabled = True
-        self.check_button: st.button or None = None
         self.head_container = st.container()
 
-        self.upload_container = st.container()
-        self.result_container = st.container()
-        self.result_container.write("")
-        self.button_container = st.container()
-        self.data_container = st.container()
-        self.data_container.write("")
+        self.draw_head()
 
-        self.__draw_gui()
-
-    def __draw_gui(self) -> None:
+    def draw_head(self) -> None:
         """
         Draw base widgets of UI
 
         :return:
             None
         """
+        print(f"{SRC_PATH / 'img.png'}")
         with self.head_container:
             title, logo = st.columns([5, 1])
             title.title("Обработка обращений граждан «AAA IT»")
             # we.image(str(SRC_PATH / "img.png"), width=100, output_format="PNG")
-            logo.image(
-                str(SRC_PATH / "logo.png"), width=150, output_format="PNG"
-            )
 
-        self.draw_choose_file()
+            logo.image(str("src/logo.png"), width=200, output_format="PNG")
 
-        with self.button_container:
-            self.check_button = st.button(
-                "Проверить",
-                help="Проверить документ",
-                on_click=lambda: self.run_file_processing(),
-                disabled=self.button_disabled,
-            )
+    def draw_input(self):
+        pass
+
+
+Gui()
