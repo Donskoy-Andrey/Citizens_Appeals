bcfa1864ce5335d5a3c9a0f39a896055acf9f6a1
TMN <super.rikunen@gmail.com>
2023-11-24 09:20:55 +0300

    Refactor django files and config server and ngrok runnel for deploying app

--- project/source/django_settings/settings.py
+++ project/source/django_settings/settings.py
@@ -27,9 +27,9 @@ SECRET_KEY = (
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
-
+CSRF_TRUSTED_ORIGINS = ["https://*.ngrok-free.app"]
 # Application definition
 
 INSTALLED_APPS = [
