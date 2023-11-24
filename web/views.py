6111a320c418266e2aa5d8fc621620d7b8d356ff
Artem Medvedev <artiebears@mail.ru>
2023-11-24 10:31:03 +0300

    add styles for submit

--- web/views.py
+++ web/views.py
@@ -5,22 +5,13 @@ from project.source.string_processing.utilities import highlight_words
 from project.source.ml.inference import setup_model
 from project.source.utils import theme_to_group
 
-# ml_model = setup_model()
+ml_model = setup_model()
+theme_to_group_mapping = theme_to_group(processing=True)
 
 
 def main_win(request):
-    # executors, themes = ml_model.predict(
-    #     ["Как же я обожаю вакцины", "Камазы, бетон и дороги очень сложно ехать"]
-    # )
-    # theme_to_group_mapping = theme_to_group(processing=True)
-    #
-    # groups = [theme_to_group_mapping[theme] for theme in themes]
-    # print(
-    #     f"Исполнитель: {executors}\n",
-    #     f"Тема: {themes}\n",
-    #     f"Группа тем: {groups}\n",
-    # )
-    # print(f"{request.POST=}")
+
+    print(f"{request.POST=}")
     if "post_content" in request.POST:
         text = request.POST.get("post_content")
         print(f"{text=}")
@@ -28,6 +19,23 @@ def main_win(request):
             return render(request, "main.html")
         else:
             text = highlight_words(text)
-            return render(request, "main.html", {"text": text})
+            executors, themes = ml_model.predict(text)
+            print(f"{list(themes)=}")
+            groups = [theme_to_group_mapping[theme] for theme in themes]
+            print(
+                f"Исполнитель: {executors}\n",
+                f"Тема: {themes}\n",
+                f"Группа тем: {groups}\n",
+            )
+            return render(
+                request,
+                "main.html",
+                {
+                    "text": text,
+                    "executor": executors[0],
+                    "theme": themes[0],
+                    "group": groups[0],
+                },
+            )
     else:
         return render(request, "main.html")
